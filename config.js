321a500f678896adc36d0d1da41a808ad0081b10
Mirlon Goriya <mirlongoriya@gmail.com>
2022-06-20 12:55:49 +0530

    more changes

--- config.js
+++ config.js
@@ -3,7 +3,8 @@ var config = {
         host: 'ec2-54-80-122-11.compute-1.amazonaws.com', 	// database host
         user: 'lgjosuktjsjvng', 		// your database username
         password: 'bfa6475ddeaa49d726b66897e83d02aae4be30f45d22732fc80a680ee0f1c844', 		// database password
-        db: 'd7816v8v7j7d5t' 		// your database name
+        db: 'd7816v8v7j7d5t', 		// your database name
+        port: '5432'
     },
     // database: {
     //     host: 'localhost', 	// database host
--- demo_create_table.js
+++ demo_create_table.js
@@ -4,7 +4,9 @@ var con = mysql.createConnection({
     host: 'ec2-54-80-122-11.compute-1.amazonaws.com', 	// database host
     user: 'lgjosuktjsjvng', 		// your database username
     password: 'bfa6475ddeaa49d726b66897e83d02aae4be30f45d22732fc80a680ee0f1c844', 		// database password
-    db: 'd7816v8v7j7d5t' 		// your database name
+    db: 'd7816v8v7j7d5t',
+    port: '5432'
+    // your database name
 });
 
 con.connect(function (err) {
